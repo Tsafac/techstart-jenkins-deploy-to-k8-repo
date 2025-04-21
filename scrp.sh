74082279855e83c46a6ff5c902e38f45a3006a1a
Tsafac <ronicefabrice@gmail.com>
2025-04-21 02:39:30 +0200

    update scrp.sh

--- scrp.sh
+++ scrp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 set -x
-./home/ec2-user/ZAP_2.16.1/zap.sh -cmd -quickurl http://$(/home/ec2-user/bin/kubectl get services/myapp --namespace=securityapp -o json | /usr/bin/jq -r ".status.loadBalancer.ingress[] | .hostname") -quickprogress -quickout /tmp/zap_report.html
+/home/ec2-user/ZAP_2.16.1/zap.sh -cmd -quickurl http://$(/home/ec2-user/bin/kubectl get services/myapp --namespace=securityapp -o json | /usr/bin/jq -r ".status.loadBalancer.ingress[] | .hostname") -quickprogress -quickout /tmp/zap_report.html
 
